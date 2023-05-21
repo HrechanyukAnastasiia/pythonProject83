4dc9277b70e9936e119fa58985d951b95ec43c42
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-21 23:33:36 +0300

    1

---
+++ 21.05.py
@@ -0,0 +1,61 @@
+import random
+class Animal:
+    def __init__(self, species, name, age, health, hunger, happiness):
+        super().init()
+        self.species = species
+        self.name = name
+        self.age = age
+        self.health= health
+        self.hunger = hunger
+        self.happiness = happiness
+    def grow(self):
+        self.age += 1
+        self.health -= random.randint(0, 10)
+        self.hunger += random.randint(0, 10)
+        self.happiness -= random.randint(0, 10)
+    def eat(self):
+        if self.hunger >= 10:
+            self.health += random.randint(0, 5)
+            self.hunger -= random.randint(5, 10)
+            self.happiness += random.randint(0, 5)
+        else:
+            print(self.name, "is not hungry")
+    def play(self):
+        if self.happiness >= 5:
+            self.health+= random.randint(0, 5)
+            self.hunger + random.randint(0, 5)
+            self.happiness += random.randint(0, 5)
+        else:
+            print(self.name, "does not want to play.")
+
+    def __str__(self):
+        return f" {self.species} - {self.name} ({self.age} years old) \n" \
+               f"Health: {self.health}\nHunger: {self.hunger}\nHappiness: {self.happiness}\n"
+
+def feed_all(self):
+    for animal in self.animals:
+        animal.eat()
+
+def str(self):
+    return "\n".join([str(animal) for animal in self.animals])
+
+def save_zoo_state (zoo, day):
+    filename = f"Day_{day}.txt"
+    with open(filename, "w") as file:
+        file.write(str(zoo))
+
+
+zoo.add_animal(Animal("Lion", "Simba", 2, 60, 6, 5))
+zoo.add_animal(Animal("Giraffe", "Melman", 4, 80, 8, 7))
+zoo.add_animal(Animal("Hippo", "Gloria", 3, 70, 7, 6))
+
+
+for i in range(1, 11):
+    print("\Day {1}")
+    save_zoo_state (zoo, i)
+    zoo.feed_all()
+    zoo.play_with (zoo.animals[randon.randint(0, len (zoo.animals) - 1)])
+    zoo.grow_all()
+    print(zoo)
+    print("\n")
+print("Simulation finished")
